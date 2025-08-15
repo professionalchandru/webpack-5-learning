8eac55f35497e7fef581dc3a8bffcec991caf9a5
professionalchandru <professionalchandru@gmail.com>
2025-08-15 17:55:38 +0530

    lazy loading or dynamic import done

--- src/components/Sorting.js
+++ src/components/Sorting.js
@@ -1,13 +1,8 @@
 import { sortingComponent } from "../Common";
 
-import _ from "lodash";
-import dayjs from "dayjs";
-
 const clickHandler = () => {
-  alert(
-    `thank you for visiting ${dayjs().format(
-      "DD MM YYY"
-    )} and your bill is ${_.add(200 + 300)}`
-  );
+  import("lodash").then(({ default: _ }) => {
+    alert(`thank you for visiting. and your bill is ${_.add(200 + 300)}`);
+  });
 };
 sortingComponent.addEventListener("click", clickHandler);
